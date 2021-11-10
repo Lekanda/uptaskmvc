dd6ad2b91da6887487773ef88ce6fb5f6d760d18
lekanda <abernaolao@gmail.com>
2021-11-10 15:54:24 +0100

    Vista crear cuenta.

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,34 @@
-<h1>Crea tu Cuenta</h1>
+<div class="contenedor crear">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crear cuenta UpTask</p>
+
+        <form method="POST" action="/" class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" id="nombre" placeholder="Tu Nombre">
+            </div>
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Tu Email">
+            </div>
+            <div class="campo">
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" id="password" placeholder="Tu password">
+            </div>
+            <div class="campo">
+                <label for="password2">Repite Contraseña</label>
+                <input type="password" name="password2" id="password2" placeholder="Repite password">
+            </div>
+            <input type="submit" class="boton" value="Iniciar Sesion">
+        </form>
+
+        <div class="acciones">
+            <a href="/">¿Ya tienes cuenta? Iniciar Sesión</a>
+            <a href="/olvide">¿Olvidaste tu Contraseña?</a>
+        </div>
+
+
+    </div>
+    <!--.contenedor-sm-->
+</div>
