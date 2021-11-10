b8ef4d9592a60beb8e834acf7036c38044f0882c
lekanda <abernaolao@gmail.com>
2021-11-10 14:24:51 +0100

    Crear el formulario de Login.

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,37 @@
-<h1>Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y Administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesion</p>
+
+        <form method="POST" action="/" class="formulario">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input 
+                    type="email" 
+                    name="email" 
+                    id="email"
+                    placeholder="Tu Email"
+                >
+            </div>
+            <div class="campo">
+                <label for="password">Contraseña</label>
+                <input 
+                    type="password" 
+                    name="password" 
+                    id="password"
+                    placeholder="Tu password"
+                >
+            </div>
+            <input type="submit" class="boton" value="Iniciar Sesion">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aun no tienes una Cuenta? Obten una</a>
+            <a href="/olvide">¿Olvidaste tu Contraseña? Reestablecer password</a>
+        </div>
+
+
+    </div><!--.contenedor-sm-->
+</div>
