f81a4bcc8ff910800f1544ed8cf9cf38506d7b66
lekanda <abernaolao@gmail.com>
2021-11-10 21:25:12 +0100

    Vista, CSS y controlador de /mensaje.

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -69,8 +69,10 @@ class LoginController {
 
 
 
-    public static function mensaje(){
-        echo 'Desde mensaje';
+    public static function mensaje(Router $router){
+        $router->render('auth/mensaje',[
+            'titulo' => 'Mensaje Restablecer Cuenta'
+        ]);
     }
 
 
