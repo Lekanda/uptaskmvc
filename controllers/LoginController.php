b99056432f232a3e56142533302d84f8f051b8a0
lekanda <abernaolao@gmail.com>
2021-11-10 21:39:39 +0100

    Vista, CSS y controlador de /confirmar.

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -77,10 +77,11 @@ class LoginController {
 
 
 
-    public static function confirmar(){
-        echo 'Desde confirmar';
+    public static function confirmar(Router $router){
+        $router->render('auth/confirmar',[
+            'titulo' => 'Cuenta reestablecida'
+        ]);
     }
-
 
     
 }
---
+++ views/auth/confirmar.php
@@ -0,0 +1,11 @@
+<div class="contenedor confirmar">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Tu cuenta ha sido Reestablecida, puedes loguearte con tu nueva contraseña</p>
+        <div class="acciones">
+            <a href="/">Iniciar Sesión</a>
+        </div>
+    </div>
+    <!--.contenedor-sm-->
+    
+</div>
