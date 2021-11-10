f5120d990be951cc64b8dfbebf2046ba0f260d85
lekanda <abernaolao@gmail.com>
2021-11-10 14:04:26 +0100

    Vistas del Login.

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,14 +2,19 @@
 
 namespace Controllers;
 
+use MVC\Router;
 
 class LoginController {
-    public static function login() {
-        echo 'Desde login';
+    public static function login(Router $router) {
+
+
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             
         }
+        $router->render('auth/login',[
+            'titulo' => 'Iniciar Sesion'
+        ]);
     }
 
 
@@ -19,12 +24,17 @@ class LoginController {
     }
 
     
-    public static function crear(){
-        echo 'Desde crear';
+    public static function crear(Router $router){
+
+
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             
         }
+
+        $router->render('auth/crear',[
+            'titulo' => 'Crear Cuenta UpTask'
+        ]);
     }
 
 
