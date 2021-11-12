a9cef8f5f39465f16039fae4359d15be7a816483
lekanda <abernaolao@gmail.com>
2021-11-12 16:19:30 +0100

    CSS a la Barra del /dashboard.

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,5 +1,5 @@
 <div class="barra">
     <p>Hola: <span><?php echo $_SESSION['nombre']; ?></span></p>
 
-    <a href="/logout" class="cerra-sesion">Cerrar Sesion</a>
+    <a href="/logout" class="cerrar-sesion">Cerrar Sesion</a>
 </div>
