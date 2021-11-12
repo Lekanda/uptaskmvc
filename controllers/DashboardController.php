2f12023463df4e33928c49f34feb505df8c290cb
lekanda <abernaolao@gmail.com>
2021-11-12 15:36:31 +0100

    Crear la vista del Dashboard.

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -7,7 +7,7 @@ use MVC\Router;
 class DashboardController {
 
     public static function index(Router $router) {
-
+        session_start();
         $router->render('dashboard/index',[
 
         ]);
