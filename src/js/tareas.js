833579468d1bb772094788d5d7d1df6fe95a4f98
lekanda <abernaolao@gmail.com>
2021-11-14 11:03:39 +0100

    Java script para mostrar el formulario.

---
+++ src/js/tareas.js
@@ -0,0 +1,13 @@
+// Al tener varios archivos js en un HTML(scripts) se pueden pisar las variables unas con otras. Para eso creamos esta funcion IIFE que hace que esa variable solo este disponible en esta funcion y no pasen a otro .js unido a un script..
+(function() {
+    // Boton para mostrar el Modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        console.log('Mostrando Form');
+    }
+
+})();
+// El () final para que se ejecute automaticamente la funcion.
+/****************************  Fin IIFE ******************************/
