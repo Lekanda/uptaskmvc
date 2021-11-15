5d90da5924b19db2224d7b1c1295ecae4d61ae8f
lekanda <abernaolao@gmail.com>
2021-11-15 14:38:42 +0100

    Routing y controladores. TareaController.

---
+++ controllers/TareaController.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Controllers;
+
+class TareaController {
+
+    public static function index() {
+        echo "TareaController::index()";
+    }
+    public static function crear() {
+        echo "TareaController::crear()";
+    }
+    public static function actualizar() {
+        echo "TareaController::actualizar()";
+    }
+    public static function eliminar() {
+        echo "TareaController::eliminar()";
+    }
+}
