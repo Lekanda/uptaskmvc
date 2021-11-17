1d48d04aa668f3123784f32d5f793a2954e851be
lekanda <abernaolao@gmail.com>
2021-11-17 11:27:55 +0100

    Detectando la tarea a eliminar.

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -70,6 +70,9 @@
             btnEliminarTarea.classList.add('eliminar-tarea');
             btnEliminarTarea.dataset.idTarea = tarea.id;
             btnEliminarTarea.textContent = 'Eliminar';
+            btnEliminarTarea.ondblclick = function () { 
+                confirmarEliminarTarea({...tarea});
+            };
 
             opcionesDiv.appendChild(btnEstadoTarea);
             opcionesDiv.appendChild(btnEliminarTarea);
@@ -266,6 +269,24 @@
         }
     }
 
+
+    function confirmarEliminarTarea(tarea) { 
+        Swal.fire({
+            title: 'Eliminar tarea?',
+            showCancelButton: true,
+            confirmButtonText: 'Si',
+            cancelButtonText: 'No',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarTarea(tarea);
+            } 
+        })
+    }
+
+    async function eliminarTarea(tarea) {
+        const datos = new FormData();
+        
+    }
 
 
 
