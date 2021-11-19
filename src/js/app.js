6b91428a923ed82e5673f77260c9505ad031b5d8
lekanda <abernaolao@gmail.com>
2021-11-19 14:23:01 +0100

    Finalizar Menu Responsive.

--- src/js/app.js
+++ src/js/app.js
@@ -17,3 +17,14 @@ if(cerrarMenuBtn){
         }, 1000);
     })
 }
+
+
+// Elimina la clase de mostrar, en un tamaño de tablet y mayores
+const anchoPantalla = document.body.clientWidth;
+
+window.addEventListener('resize', function(){
+    const anchoPantalla = document.body.clientWidth;
+    if(anchoPantalla >= 768){
+        sidebar.classList.remove('mostrar');
+    }
+});
